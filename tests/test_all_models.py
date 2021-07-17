c7f4f415a46f258f4bf16eb6b94a4589e609a82c
jack89roberts <jroberts@turing.ac.uk>
2021-07-17 23:04:39 +0100

    add predict_score_n_proba  and predict_concede_n_proba

--- tests/test_all_models.py
+++ tests/test_all_models.py
@@ -3,6 +3,7 @@ import jax.numpy as jnp
 import pytest
 
 from bpl import DixonColesMatchPredictor, ExtendedDixonColesMatchPredictor
+from bpl.base import MAX_GOALS
 
 MODELS = [DixonColesMatchPredictor, ExtendedDixonColesMatchPredictor]
 
@@ -42,3 +43,48 @@ def test_predict_outcome_proba(dummy_data, model_cls):
         "draw"
     ] == pytest.approx(1.0, abs=1e-5)
 
+
+@pytest.mark.parametrize("model_cls", MODELS)
+def test_predict_score_n_proba(dummy_data, model_cls):
+    model = model_cls().fit(dummy_data, num_samples=100, num_warmup=100)
+
+    proba_home = jnp.array(
+        [
+            model.predict_score_n_proba(n, "0", "1")
+            for n in range(MAX_GOALS + 1)
+        ]
+    )
+    assert jnp.all((proba_home >= 0) & (proba_home <= 1))
+    assert sum(proba_home) == pytest.approx(1.0, abs=1e-5)
+
+    proba_away = jnp.array(
+        [
+            model.predict_score_n_proba(n, "0", "1", home=False)
+            for n in range(MAX_GOALS + 1)
+        ]
+    )
+    assert jnp.all((proba_away >= 0) & (proba_away <= 1))
+    assert sum(proba_away) == pytest.approx(1.0, abs=1e-5)
+
+
+@pytest.mark.parametrize("model_cls", MODELS)
+def test_predict_concede_n_proba(dummy_data, model_cls):
+    model = model_cls().fit(dummy_data, num_samples=100, num_warmup=100)
+
+    proba_home = jnp.array(
+        [
+            model.predict_concede_n_proba(n, "0", "1")
+            for n in range(MAX_GOALS + 1)
+        ]
+    )
+    assert jnp.all((proba_home >= 0) & (proba_home <= 1))
+    assert sum(proba_home) == pytest.approx(1.0, abs=1e-5)
+
+    proba_away = jnp.array(
+        [
+            model.predict_concede_n_proba(n, "0", "1", home=False)
+            for n in range(MAX_GOALS + 1)
+        ]
+    )
+    assert jnp.all((proba_away >= 0) & (proba_away <= 1))
+    assert sum(proba_away) == pytest.approx(1.0, abs=1e-5)
