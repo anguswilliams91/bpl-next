81217ea0048168d10c6873a767cecf903a8895bb
jack89roberts <jroberts@turing.ac.uk>
2022-07-19 09:18:25 +0100

    convert int goals to np.array

--- bpl/_util.py
+++ bpl/_util.py
@@ -1,18 +1,25 @@
 """Private utility functions."""
+from typing import Iterable, Union
+
 import jax
 import jax.numpy as jnp
 import numpy as np
 
 
 def dixon_coles_correlation_term(
-    home_goals: np.array,
-    away_goals: np.array,
+    home_goals: Union[int, Iterable[int]],
+    away_goals: Union[int, Iterable[int]],
     home_rate: jnp.array,
     away_rate: jnp.array,
     corr_coef: jnp.array,
     tol: float = 0,  # FIXME workaround to clip negative values to tol to avoid NaNs
 ) -> jnp.array:
     # correlation term from dixon and coles paper
+    if isinstance(home_goals, int):
+        home_goals = np.array(home_goals).reshape((1,))
+    if isinstance(away_goals, int):
+        away_goals = np.array(away_goals).reshape((1,))
+
     corr_term = jnp.zeros_like(home_rate)
 
     nil_nil = (home_goals == 0) & (away_goals == 0)
