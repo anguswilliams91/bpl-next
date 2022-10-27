ddaf27e88a45c24df8c0b759f9dfe50a2d9b9848
jack89roberts <jroberts@turing.ac.uk>
2022-10-27 08:31:29 +0100

    import neutral model in init

--- bpl/__init__.py
+++ bpl/__init__.py
@@ -3,3 +3,4 @@ __version__ = "0.2.0"
 
 from bpl.dixon_coles import DixonColesMatchPredictor
 from bpl.extended_dixon_coles import ExtendedDixonColesMatchPredictor
+from bpl.neutral_dixon_coles import NeutralDixonColesMatchPredictor
