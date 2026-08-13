039214beb3a1f669eb7ad9405f0ac7af53b5c248
nbarlowATI <nbarlow@turing.ac.uk>
2026-08-13 16:11:30 +0100

    fix packagenotfounderror downstream due to project name change

--- bpl/__init__.py
+++ bpl/__init__.py
@@ -1,4 +1,4 @@
-from importlib.metadata import version
+from importlib.metadata import version, PackageNotFoundError
 
 from bpl.dixon_coles import DixonColesMatchPredictor
 from bpl.extended_dixon_coles import ExtendedDixonColesMatchPredictor
